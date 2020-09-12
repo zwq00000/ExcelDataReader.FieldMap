21ecaef70e7863dfa2477debd593f578c6bab9c8
zwq00000 <zwq00000@hotmail.com>
2020-09-12 19:08:40 +0800

    增加 测试

---
+++ test/ExcelDataReader.FieldMaps.Tests/Fillreport/ReportDtoParser.cs
@@ -0,0 +1,24 @@
+﻿namespace ExcelDataReader.FieldMaps.Tests {
+    public class ReportDtoParser : ExcelFileParser<ReportDto> {
+        private static readonly FieldMapBuilder<ReportDto> builder = FieldMapBuilder<ReportDto>
+        .Create ("Row", s => s.Row)
+        .Add ("Account Number", r => r.AccountNumber)
+        .Add ("Trader", r => r.Trader)
+        .Add ("Order Number", r => r.OrderNumber)
+        .Add ("Leg Number", r => r.LegNumber)
+        .Add ("Buy Or Sell", r => r.BuyOrSell)
+        .Add ("Quantity", r => r.Quantity)
+        .Add ("Contract", r => r.Contract)
+        .Add ("Fill Price", r => r.FillPrice)
+        .Add ("Fill Price Decimal", r => r.FillPriceDecimal)
+        .Add ("Fill Time", r => r.FillTime);
+
+        private static ReadSettings settings = new ReadSettings(){
+            RowNumberField = "Row",
+            StartRow = 1
+        };
+
+        public ReportDtoParser () : base (builder,settings) { }
+
+    }
+}
